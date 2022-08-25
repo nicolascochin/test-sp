cca1dc61bb8d789f70c6cd0bd99063cb9a78aaf3
Nicolas COCHIN <github@account.rocks>
2022-08-25 17:01:07 +0200

    Add tests constants

--- test/abstract_test.rb
+++ test/abstract_test.rb
@@ -3,6 +3,8 @@ require 'minitest/autorun'
 ROOT_PATH=File.dirname(__dir__)
 APP_PATH="#{ROOT_PATH}/app"
 TEST_PATH="#{ROOT_PATH}/test"
+FIXTURES_PATH="#{TEST_PATH}/fixtures"
+EXPECTATIONS_PATH="#{TEST_PATH}/expectations"
 
 class AbstractTest < Minitest::Test
 end
