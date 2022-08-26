4c086ae8fa60876306a1cfb1e9a55cf5195f0704
Nicolas COCHIN <github@account.rocks>
2022-08-26 11:41:32 +0200

    Add a warning when the row is invalid

--- test/unit/row_entry_builder_test.rb
+++ test/unit/row_entry_builder_test.rb
@@ -7,6 +7,7 @@ require_relative "#{APP_PATH}/row_entry"
 class RowEntryBuilderTest < AbstractTest
   def setup
     @valid_input = 'an_endpoint an_ip'
+    @invalid_input = 'invalid_input_ip_is_missing'
   end
 
   def test_an_empty_string_returns_nil
@@ -18,7 +19,13 @@ class RowEntryBuilderTest < AbstractTest
   end
 
   def test_an_invalid_string_returns_nil
-    assert_nil RowEntryBuilder.call('invalid_input_ip_is_missing')
+    assert_nil RowEntryBuilder.call(@invalid_input)
+  end
+
+  def test_an_invalid_string_writes_a_warning
+    out, err = capture_io { RowEntryBuilder.call(@invalid_input) }
+    assert_empty out
+    assert_equal 'Invalid entry, skipping the row', err.chomp
   end
 
   def test_a_valid_string_returns_a_row_entry
