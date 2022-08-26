72aeb820e0ea272e2f7d95696f461bbb82f18d01
Nicolas COCHIN <github@account.rocks>
2022-08-26 11:22:59 +0200

    add test for empty string

--- test/unit/row_entry_builder_test.rb
+++ test/unit/row_entry_builder_test.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 require_relative '../abstract_test'
+require_relative "#{APP_PATH}/row_entry_builder"
 
 class RowEntryBuilderTest < AbstractTest
+  def test_an_empty_string_returns_nil
+    assert_nil RowEntryBuilder.call('')
+  end
 end
