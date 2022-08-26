98661fbf7222e9605ea139fc4b3a1edcc1c84f38
Nicolas COCHIN <github@account.rocks>
2022-08-26 11:55:29 +0200

    add test when no entry is added

--- test/unit/row_entry_aggregator_test.rb
+++ test/unit/row_entry_aggregator_test.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 require_relative '../abstract_test'
+require_relative "#{APP_PATH}/row_entry_aggregator"
 
 class RowEntryAggregatorTest < AbstractTest
+  def setup
+    @aggregator = RowEntryAggregator.new
+  end
+
+  def test_returns_an_empty_hash_when_no_entry_is_added
+    assert_equal({}, @aggregator.data)
+  end
 end
