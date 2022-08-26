0f2e3e853afa7ea4fae15a44f7cafe336c78abc3
Nicolas COCHIN <github@account.rocks>
2022-08-26 12:02:48 +0200

    add test when a row entry is added

--- test/unit/row_entry_aggregator_test.rb
+++ test/unit/row_entry_aggregator_test.rb
@@ -2,13 +2,23 @@
 
 require_relative '../abstract_test'
 require_relative "#{APP_PATH}/row_entry_aggregator"
+require_relative "#{APP_PATH}/row_entry"
 
 class RowEntryAggregatorTest < AbstractTest
   def setup
     @aggregator = RowEntryAggregator.new
+    @row_entry = RowEntry.new(endpoint: 'an_endpoint', ip: 'an_ip')
   end
 
   def test_returns_an_empty_hash_when_no_entry_is_added
     assert_equal({}, @aggregator.data)
+  end
+
+  def test_a_row_entry_is_added
+    @aggregator.add_row_entry(@row_entry)
+    assert_equal(
+      { 'an_endpoint' => ['an_ip'] },
+      @aggregator.data
+    )
   end
 end
