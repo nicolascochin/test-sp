ed588023d834999f8193caf7d8a82d003dc7bdeb
Nicolas COCHIN <github@account.rocks>
2022-08-26 13:49:49 +0200

    Add test when no aggregator is given

--- test/unit/unique_views_printer_test.rb
+++ test/unit/unique_views_printer_test.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 require_relative '../abstract_test'
+require_relative "#{APP_PATH}/unique_views_printer"
 
 class UniqueViewsPrinterTest < AbstractTest
+  def test_a_nil_entry_does_not_print_any_result
+    expected = <<~NO_RESULT
+      Unique views:
+      no data
+    NO_RESULT
+    out, err = capture_io { UniqueViewsPrinter.call(nil) }
+    assert_equal expected, out
+    assert_equal '', err
+  end
 end
