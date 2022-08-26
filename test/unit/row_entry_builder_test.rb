735619817a98e4561303659ae6f9f73741371141
Nicolas COCHIN <github@account.rocks>
2022-08-26 11:24:28 +0200

    add test for a nil entry

--- test/unit/row_entry_builder_test.rb
+++ test/unit/row_entry_builder_test.rb
@@ -7,4 +7,8 @@ class RowEntryBuilderTest < AbstractTest
   def test_an_empty_string_returns_nil
     assert_nil RowEntryBuilder.call('')
   end
+
+  def test_a_nil_entry_returns_nil
+    assert_nil RowEntryBuilder.call(nil)
+  end
 end
