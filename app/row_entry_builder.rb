6fcfa0bbe2c862ed3dae1aea389d8791962d5c31
Nicolas COCHIN <github@account.rocks>
2022-08-26 11:31:36 +0200

    Fix failure

--- app/row_entry_builder.rb
+++ app/row_entry_builder.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
 
 class RowEntryBuilder
-  def self.call(_raw_string)
-    nil
+  def self.call(raw_string)
+    return nil if raw_string.nil? || raw_string.empty?
+
+    raw_string
+      .split
+      .then do |row_array|
+        row_array.size == 2 ? RowEntry.new(endpoint: row_array[0], ip: row_array[1]) : nil
+      end
   end
 end
