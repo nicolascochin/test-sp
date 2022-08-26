af906aed1fc544699777ff2e1a5feda1f1eec5ed
Nicolas COCHIN <github@account.rocks>
2022-08-26 12:42:32 +0200

    Fix failure

--- app/most_page_views_printer.rb
+++ app/most_page_views_printer.rb
@@ -1,8 +1,23 @@
 # frozen_string_literal: true
 
 class MostPageViewsPrinter
-  def self.call(_aggregator)
-    puts 'Most page views:'
-    puts 'no data'
+  class << self
+    def call(aggregator)
+      puts 'Most page views:'
+      if aggregator.nil?
+        puts 'no data'
+        return
+      end
+      print_sorted_data(aggregator)
+    end
+
+    private
+
+    def print_sorted_data(aggregator)
+      aggregator
+        .sort_by { |_endpoint, ips| ips.count }
+        .reverse_each
+        .each { |endpoint, ips| puts "#{endpoint} #{ips.count} visits" }
+    end
   end
 end
