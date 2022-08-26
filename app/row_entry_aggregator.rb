2c461a9922191a2ab25510420a21a23b40e1e4d6
Nicolas COCHIN <github@account.rocks>
2022-08-26 12:04:24 +0200

    Fix failure

--- app/row_entry_aggregator.rb
+++ app/row_entry_aggregator.rb
@@ -4,6 +4,10 @@ class RowEntryAggregator
   attr_reader :data
 
   def initialize
-    @data = {}
+    @data = Hash.new { |hash, k| hash[k] = [] unless hash.key?(k) }
+  end
+
+  def add_row_entry(row_entry)
+    @data[row_entry.endpoint] << row_entry.ip
   end
 end
